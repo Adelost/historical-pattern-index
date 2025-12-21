570b6e11add3831e486508b092f3cd40c4423b15
Mattias Wetterlind <adelost@gmail.com>
2025-12-21 11:49:15 +0100

    Redesign tier colors and use tier-color for deaths
    Color scheme changes:
    - Deaths now display in tier color instead of always red
    - Tier colors follow warm→cool spectrum (intentional→chaotic):
      - Erasure: red (worst, blood)
      - Industrial: peach (warning, machinery)
      - Profit: yellow (greed, gold)
      - Collapse: sapphire (cold, impersonal)
      - Chaotic: lavender (diffuse, unclear)
    
    Updated components:
    - TableRow, MapPopup, EventTooltip, Card death displays
    - Tier filter pills match new colors

--- js/utils.js
+++ js/utils.js
@@ -22,30 +22,31 @@ export const DRIVERS = {
 };
 
 // Tier colors (Catppuccin Mocha)
+// Spectrum: warm (intentional) → cool (chaotic)
 export const THEME = {
     tiers: {
         'TOTAL ERASURE': {
-            color: '#eba0ac',  // maroon - blood, ethnic cleansing
+            color: '#f38ba8',  // red - blood, total destruction (worst)
             shortLabel: 'Erasure',
             icon: '<path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-5-9h10v2H7z"/>'
         },
         'INDUSTRIAL MEGA-EVENT': {
-            color: '#f38ba8',  // red - ideology, industrialized killing
+            color: '#fab387',  // peach - warning, machinery, bureaucracy
             shortLabel: 'Industrial',
             icon: '<path d="M12 7V3H2v18h20V7H12zM6 19H4v-2h2v2zm0-4H4v-2h2v2zm0-4H4V9h2v2zm0-4H4V5h2v2zm4 12H8v-2h2v2zm0-4H8v-2h2v2zm0-4H8V9h2v2zm0-4H8V5h2v2zm10 12h-8v-2h2v-2h-2v-2h2v-2h-2V9h8v10z"/>'
         },
-        'CONTINENTAL COLLAPSE': {
-            color: '#6c7086',  // overlay0 - war, empire collapse
-            shortLabel: 'Collapse',
-            icon: '<path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>'
-        },
         'PROFIT-DRIVEN ATTRITION': {
-            color: '#f9e2af',  // yellow - profit, greed, extraction
+            color: '#f9e2af',  // yellow - profit, greed, gold
             shortLabel: 'Profit',
             icon: '<path d="M11.8 10.9c-2.27-.59-3-1.2-3-2.15 0-1.09 1.01-1.85 2.7-1.85 1.78 0 2.44.85 2.5 2.1h2.21c-.07-1.72-1.12-3.3-3.21-3.81V3h-3v2.16c-1.94.42-3.5 1.68-3.5 3.61 0 2.31 1.91 3.46 4.7 4.13 2.5.6 3 1.48 3 2.41 0 .69-.49 1.79-2.7 1.79-2.06 0-2.87-.92-2.98-2.1h-2.2c.12 2.19 1.76 3.42 3.68 3.83V21h3v-2.15c1.95-.37 3.5-1.5 3.5-3.55 0-2.84-2.43-3.81-4.7-4.4z"/>'
         },
+        'CONTINENTAL COLLAPSE': {
+            color: '#74c7ec',  // sapphire - cold, impersonal, war
+            shortLabel: 'Collapse',
+            icon: '<path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/>'
+        },
         'CHAOTIC ATROCITY': {
-            color: '#585b70',  // surface2 - chaotic, opportunistic
+            color: '#b4befe',  // lavender - diffuse, chaotic, unclear
             shortLabel: 'Chaotic',
             icon: '<path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"/>'
         }
